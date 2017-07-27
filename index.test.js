9f8b429fcd9ec97bbfd608c3666b6093d2b296a8
Tiaan <tiaanduplessis@hotmail.com>
2017-07-27 11:03:06 +0200

    INIT

---
+++ index.test.js
@@ -0,0 +1,9 @@
+const { buffer, text, json } = require('./')
+
+test('should be defined', () => {
+  expect(buffer).toBeDefined()
+  expect(text).toBeDefined()
+  expect(json).toBeDefined()
+})
+
+test()
